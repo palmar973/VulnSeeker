acb13644b692027d45231e6714353cefe281e1b8
Claudio <palmarclaudio187@gmail.com>
2025-12-21 14:37:38 -0400

    Feat: Inicio del sistema VulnSeeker (Hola Mundo)

---
+++ main.py
@@ -0,0 +1,10 @@
+import sys
+
+def iniciar_vulnseeker():
+    print("🦅 SISTEMA VULNSEEKER: ONLINE")
+    print(f"🐍 Versión del Núcleo: Python {sys.version.split()[0]}")
+    print("📂 Directorio de Misión: Establecido.")
+    print("🚀 Comandante Claudio: Listo para el despliegue.")
+
+if __name__ == "__main__":
+    iniciar_vulnseeker()
